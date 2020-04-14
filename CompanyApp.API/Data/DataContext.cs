26460c23b88df1f5648681b1fbaba88646008d06
victor shankar <victorshankar@live.com>
2020-04-14 15:09:26 -0700

    Initial

---
+++ CompanyApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using CompanyApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
