70c75d25570ea4b73d6daa881454362416074823
victor shankar <victorshankar@live.com>
2020-04-19 17:50:58 -0700

    End of Section 3

---
+++ CompanyApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage ="Password must be between 4 and 8 characters ")]
+        public string Password { get; set; }
+    }
+}
