1cc4ce8d62dd1a32116579394ec9a679a1c70643
victor shankar <victorshankar@live.com>
2020-04-28 08:25:11 -0700

    End of section 4

--- CompanyApp.API/Controllers/ValuesController.cs
+++ CompanyApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace CompanyApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult>  GetValues()
         {
