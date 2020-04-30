087ae8d90bcce2f098270e51d21afb7d42b53f71
victor shankar <victorshankar@live.com>
2020-04-30 09:42:20 -0700

    end of section 5

--- CompanyApp-SPA/src/app/app.module.ts
+++ CompanyApp-SPA/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
    declarations: [
-      AppComponent,      
+      AppComponent,
       NavComponent,
       HomeComponent,
       RegisterComponent
@@ -21,7 +22,8 @@ import { RegisterComponent } from './register/register.component';
       FormsModule
    ],
    providers: [
-      AuthService
+      AuthService,
+      ErrorInterceptorProvider
    ],
    bootstrap: [
       AppComponent
